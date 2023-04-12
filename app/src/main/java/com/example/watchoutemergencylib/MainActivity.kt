96d00eb0503d3686b203dc39e42605c0cdb8b004
Rahul kumar <rahul.kumar@aryavratinfotech.com>
2023-04-12 16:58:29 +0530

    fix bug

--- app/src/main/java/com/example/watchoutemergencylib/MainActivity.kt
+++ app/src/main/java/com/example/watchoutemergencylib/MainActivity.kt
@@ -11,9 +11,10 @@ import com.example.watchoutemergencylib.databinding.ActivityMainBinding
 import org.json.JSONArray
 import org.json.JSONObject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
 
     lateinit var binding: ActivityMainBinding
+    var numbers: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,6 +22,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        binding.goforEmergency.setOnClickListener(this)
     }
 
+
+    @SuppressLint("SuspiciousIndentation")
+    override fun onClick(v: View?) {
+
+        if (v?.id == R.id.gofor_emergency) {
+
+            var jsonStr="{\n" +
+                    "  \"apiAuthrizationKey\": \"1221117105526941\",\n" +
+                    "  \"longitude\": 75.7873,\n" +
+                    "  \"latitude\": 26.9124,\n" +
+                    "  \"tripBy\": \"ola\",\n" +
+                    "  \"userType\": \"p\",\n" +
+                    "  \"user\": {\n" +
+                    "    \"firstName\": \"vicky\",\n" +
+                    "    \"lastName\": \"kumar\",\n" +
+                    "    \"email\": \"vicky3i@gmail.com\",\n" +
+                    "    \"phone\": \"85856895885\",\n" +
+                    "    \"gender\": \"Male\"\n" +
+                    "    \n" +
+                    "  },\n" +
+                    "  \"vehicle\": {\n" +
+                    "    \"vehYear\": \"2014\",\n" +
+                    "    \"vehMake\": \"Ford\",\n" +
+                    "    \"vinNum\": \"1FADP3J27EL153248\",\n" +
+                    "    \"vehicle_model\": \"Focus\",\n" +
+                    "    \"vehicle_number\": \"RJ16CH7898\",\n" +
+                    "    \"vehicleType\": \"Ambulance\"\n" +
+                    "  }\n" +
+                    "}"
+
+
+          val intent = Intent(this, WatchOutEmergency::class.java)
+            intent.putExtra("userinfo",jsonStr);
+            startActivity(intent)
+        }
+
+    }
 }
